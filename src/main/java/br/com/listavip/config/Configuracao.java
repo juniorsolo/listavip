3bfe209e37e48cb3acda06cb580cf6fca3cf8399
juniorsolo <junior.solo@hotmail.com>
2018-08-16 21:47:10 -0300

    Criando arquivo de propriedades do spring e adicionando configurações do dataSource.

--- src/main/java/br/com/listavip/config/Configuracao.java
+++ src/main/java/br/com/listavip/config/Configuracao.java
@@ -1,14 +1,10 @@
 package br.com.listavip.config;
 
-import javax.sql.DataSource;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @SpringBootApplication
 @ComponentScan(basePackages= {"br.com.listavip.controler","br.com.listavip.service"})
@@ -18,21 +14,6 @@ public class Configuracao {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Configuracao.class, args);
-	}
-	
-	@Bean
-	public DataSource dataSource() {
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/listavip");
-		dataSource.setUsername("root");
-		dataSource.setPassword("root");
-		return dataSource;
-	}
-	
-	
-	
-	
-	
+	}	
 	
 }
